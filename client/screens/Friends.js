a422ce0658d0db28232606a1c55325d4c9f262c1
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-04-25 18:36:16 -0500

    friends list

--- client/screens/Friends.js
+++ client/screens/Friends.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   Body,
@@ -8,14 +8,97 @@ import {
   Button,
   Icon,
   Right,
+  List,
+  ListItem,
+  Thumbnail,
   Text,
+  Segment,
   Left,
 } from "native-base";
 
 const Friends = (props) => {
+  const [view, setView] = useState(0);
+
+  const renderComponent = () => {
+    if (view === 0)
+      return (
+        <List>
+          <ListItem avatar noBorder>
+            <Left>
+              <Thumbnail
+                source={{
+                  uri:
+                    "https://style.anu.edu.au/_anu/4/images/placeholders/person.png",
+                }}
+              />
+            </Left>
+            <Body>
+              <Text>Eugene Fitzgerald</Text>
+            </Body>
+            <Right>
+              <Icon name="md-arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem avatar noBorder>
+            <Left>
+              <Thumbnail
+                source={{
+                  uri:
+                    "https://style.anu.edu.au/_anu/4/images/placeholders/person.png",
+                }}
+              />
+            </Left>
+            <Body>
+              <Text>Claire Saffitz</Text>
+            </Body>
+            <Right>
+              <Icon name="md-arrow-forward" />
+            </Right>
+          </ListItem>
+        </List>
+      );
+    else
+      return (
+        <List>
+          <ListItem avatar noBorder>
+            <Left>
+              <Thumbnail
+                source={{
+                  uri:
+                    "https://style.anu.edu.au/_anu/4/images/placeholders/person.png",
+                }}
+              />
+            </Left>
+            <Body>
+              <Text>Jane Porter</Text>
+            </Body>
+            <Right>
+              <Icon name="md-arrow-forward" />
+            </Right>
+          </ListItem>
+          <ListItem avatar noBorder>
+            <Left>
+              <Thumbnail
+                source={{
+                  uri:
+                    "https://style.anu.edu.au/_anu/4/images/placeholders/person.png",
+                }}
+              />
+            </Left>
+            <Body>
+              <Text>Mulan Fa</Text>
+            </Body>
+            <Right>
+              <Icon name="md-arrow-forward" />
+            </Right>
+          </ListItem>
+        </List>
+      );
+  };
+
   return (
     <Container>
-      <Header>
+      <Header hasSegment>
         <Left>
           <Button transparent onPress={() => props.navigation.goBack()}>
             <Icon name="arrow-round-back" />
@@ -28,9 +111,15 @@ const Friends = (props) => {
           <Icon name="people" />
         </Right>
       </Header>
-      <Content padder>
-        <Text>list</Text>
-      </Content>
+      <Segment>
+        <Button first active={view === 0} onPress={() => setView(0)}>
+          <Text>Followers</Text>
+        </Button>
+        <Button last active={view === 1} onPress={() => setView(1)}>
+          <Text>Following</Text>
+        </Button>
+      </Segment>
+      <Content padder>{renderComponent()}</Content>
     </Container>
   );
 };
