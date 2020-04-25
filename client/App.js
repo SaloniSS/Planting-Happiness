97b7b0b29fbd89beb2054911169ff26cdaf04825
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-04-25 09:38:51 -0500

    fix font issue

--- client/App.js
+++ client/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import {YellowBox} from 'react-native';
+import React from "react";
 
 // Screens
 import LoginScreen from "./screens/LoginScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import TabBar from "./components/TabBar";
-import { Font } from 'expo';
-import { Ionicons } from '@expo/vector-icons';
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
 //React Navigation Setup
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
@@ -25,16 +24,14 @@ const AppContainer = createAppContainer(MainNavigator);
 class App extends React.Component {
   async componentDidMount() {
     await Font.loadAsync({
-      'Roboto': require('native-base/Fonts/Roboto.ttf'),
-      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
       ...Ionicons.font,
     });
   }
 
   render() {
-    return ( 
-        <AppContainer/>
-    );
+    return <AppContainer />;
   }
 }
 
