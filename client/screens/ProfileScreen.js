0efe542b377c93b58cd0395cf7106f9cc392d1e3
SaloniSS <sxs170015@utdallas.edu>
2020-04-25 08:34:07 -0500

    Attempt to bring sign out back

--- client/screens/ProfileScreen.js
+++ client/screens/ProfileScreen.js
@@ -1,54 +1,34 @@
-import React from "react";
-import {
-  Container,
-  Header,
-  Body,
-  Title,
-  Button,
-  Row,
-  Grid,
-  Form,
-  Item,
-  Input,
-  Col,
-  Label,
-  Text,
-} from "native-base";
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Button } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import CircleSlider from "../components/CircleSlider";
+export default class ProfileScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text> Profile Screen </Text>
+        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+          Welcome, {this.props.navigation.getParam("username")}
+        </Text>
+        <Button
+          title="Go To Account Set-Up"
+          onPress={() => this.props.navigation.navigate('AccountSetUp')}
+        />
+        <Button
+          title="Sign out"
+          onPress={() => this.props.navigation.navigate("Login")}
+        />
+      </View>
+    );
+  }
+}
 
-const ProfileScreen = (props) => {
-  // don't use content because it is a ScrollView
-  return (
-    <Container>
-      <Header>
-        <Body>
-          <Title>Set Up Account</Title>
-        </Body>
-      </Header>
-      <Form>
-        <Item floatingLabel last>
-          <Label>Username</Label>
-          <Input />
-        </Item>
-        <Text>Set a daily point goal:</Text>
-        <Item>
-          <CircleSlider
-            startGradient="#B5EAD7"
-            endGradient="#C7CEEA"
-            value={100}
-            onValueChange={(value) => console.log(value)}
-          />
-        </Item>
-      </Form>
-      <Button
-        onPress={() => props.navigation.navigate("Feed")}
-        style={{ alignSelf: "flex-start" }}
-      >
-        <Text>Continue</Text>
-      </Button>
-    </Container>
-  );
-};
-
-export default ProfileScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
