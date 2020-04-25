0c523016623944c055c5a84ad5b2a9074b0df26c
meganjtran <meganjtran@gmail.com>
2020-04-25 08:25:21 -0600

    making everything beautiful
    wow

--- client/screens/LoginScreen.js
+++ client/screens/LoginScreen.js
@@ -1,10 +1,61 @@
 import React, { Component } from "react";
 import * as Google from "expo-google-app-auth";
 import { IOS_ID, ANDROID_ID } from "./ClientID";
-import { Container, Content, Button, Text } from "native-base";
+import { Container, Content, Button, Text, View } from "native-base";
+import {ImageBackground, Image, StyleSheet} from "react-native";
+import Swiper from 'react-native-swiper'
+import LinearGradient from 'react-native-linear-gradient';
 
 const IOS_CLIENT_ID = IOS_ID;
 const ANDROID_CLIENT_ID = ANDROID_ID;
+
+import gradientBkgd from '../assets/homeBkgd.png';
+import leaf from '../assets/leafBoi.png';
+
+var styles = {
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+    //backgroundColor: '#b5ead7'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+    //backgroundColor: '#bfd9e1'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+    //backgroundColor: '#c7ceea'
+  },
+  heading: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  desc: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+    padding: 15
+  },
+  bkgdImg: {
+    resizeMode: 'stretch',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  leafImg: {
+    width: 70,
+    height: 85  
+  }
+}
+
 
 const LoginScreen = (props) => {
   const signInWithGoogle = async () => {
@@ -31,17 +82,32 @@ const LoginScreen = (props) => {
   };
 
   return (
-    <Container>
-      <Content
-        padder
-        contentContainerStyle={{ justifyContent: "center", flex: 1 }}
-      >
-        <Button onPress={signInWithGoogle} style={{ alignSelf: "flex-start" }}>
-          <Text>Login with Google</Text>
+    <Swiper style={styles.wrapper} showsButtons loop={false}>
+    <View testID="Hello" style={styles.slide1}>
+      <ImageBackground source={gradientBkgd} style={styles.bkgdImg}>
+        <Image source={leaf} style={styles.leafImg}></Image>
+        <Text style={styles.heading}>Welcome to</Text>
+        <Text style={styles.heading}>Planting Happiness ♥</Text>
+      </ImageBackground>
+    </View>
+    <View testID="Tagline" style={styles.slide2}>
+      <ImageBackground source={gradientBkgd} style={styles.bkgdImg}>
+        <Text style={styles.desc}>Set goals for exercise, eating healthy, productivity, and so much more!</Text>
+      </ImageBackground>
+    </View>
+    <View testID="Info" style={styles.slide3}>
+      <ImageBackground source={gradientBkgd} style={styles.bkgdImg}>
+        <Text style={styles.desc}>Connect with peers and share your activities!</Text>
+        <Text style={styles.desc}>Gain points to redeem for cool prizes!</Text>
+        <Button onPress={signInWithGoogle} style={{alignItems: "center", justifyContent: "center" }}>
+          <Text>Sign in with Google</Text>
         </Button>
-      </Content>
-    </Container>
+      </ImageBackground>
+    </View>
+  </Swiper>
   );
 };
 
 export default LoginScreen;
+
+
--- client/screens/ProfileScreen.js
+++ client/screens/ProfileScreen.js
@@ -14,39 +14,53 @@ import {
   Label,
   Text,
 } from "native-base";
-
+import {ImageBackground} from "react-native";
 import CircleSlider from "../components/CircleSlider";
+
+import gradientBkgd from '../assets/mainBkgd.png';
+
+var styles = {
+  bkgdImg: {
+    resizeMode: 'stretch',
+    justifyContent: 'center',
+    alignContent: 'space-around',
+    width: '100%',
+    height: '100%'
+  }
+}
 
 const ProfileScreen = (props) => {
   // don't use content because it is a ScrollView
   return (
     <Container>
-      <Header>
-        <Body>
-          <Title>Set Up Account</Title>
-        </Body>
-      </Header>
-      <Form>
-        <Item floatingLabel last>
-          <Label>Username</Label>
-          <Input />
-        </Item>
-        <Text>Set a daily point goal:</Text>
-        <Item>
-          <CircleSlider
-            startGradient="#B5EAD7"
-            endGradient="#C7CEEA"
-            value={100}
-            onValueChange={(value) => console.log(value)}
-          />
-        </Item>
-      </Form>
-      <Button
-        onPress={() => props.navigation.navigate("Feed")}
-        style={{ alignSelf: "flex-start" }}
-      >
-        <Text>Continue</Text>
-      </Button>
+      <ImageBackground source={gradientBkgd} style={styles.bkgdImg}>
+        <Header>
+          <Body>
+            <Title>Set Up Account</Title>
+          </Body>
+        </Header>
+        <Form>
+          <Item floatingLabel last>
+            <Label>Username</Label>
+            <Input />
+          </Item>
+          <Text>Set a daily point goal:</Text>
+          <Item>
+            <CircleSlider
+              startGradient="#B5EAD7"
+              endGradient="#C7CEEA"
+              value={100}
+              onValueChange={(value) => console.log(value)}
+            />
+          </Item>
+        </Form>
+        <Button
+          onPress={() => props.navigation.navigate("Feed")}
+          style={{ alignSelf: "flex-start" }}
+        >
+          <Text>Continue</Text>
+        </Button>
+      </ImageBackground>
     </Container>
   );
 };
