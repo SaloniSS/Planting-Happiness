cc0af92b9a97d2f9e4c61436b7d044cbc9738402
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-04-24 20:03:06 -0500

    other stuff

--- client/App.js
+++ client/App.js
@@ -1,20 +1,11 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from "react";
 
-import HomePage from './screens/HomePage';
-
-const Stack = createStackNavigator();
+import HomePage from "./screens/HomePage";
+import AddActivity from "./screens/AddActivity";
+import TabBar from "./components/TabBar";
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomePage} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AddActivity />;
 }
 
 export default App;
