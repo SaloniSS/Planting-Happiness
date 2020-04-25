018a003ea1a2a8f4a2873ce55076aa332dd72874
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-04-25 12:18:46 -0500

    get back button working

--- client/screens/Statistics.js
+++ client/screens/Statistics.js
@@ -1,13 +1,32 @@
 import React from "react";
-import { Container, Body, Content, Header, Title, Text } from "native-base";
+import {
+  Container,
+  Body,
+  Content,
+  Header,
+  Title,
+  Button,
+  Icon,
+  Right,
+  Text,
+  Left,
+} from "native-base";
 
-const Statistics = () => {
+const Statistics = (props) => {
   return (
     <Container>
       <Header>
+        <Left>
+          <Button transparent onPress={() => props.navigation.goBack()}>
+            <Icon name="arrow-round-back" />
+          </Button>
+        </Left>
         <Body>
           <Title>Statistics</Title>
         </Body>
+        <Right>
+          <Icon name="md-stats" />
+        </Right>
       </Header>
       <Content padder>
         <Text>content</Text>
