709d02bb103f86c78d9509b3aeb630e403b640f1
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-04-25 18:56:02 -0500

    add toast after submitting

--- client/screens/AddActivity.js
+++ client/screens/AddActivity.js
@@ -14,6 +14,8 @@ import {
   Text,
   Segment,
   Form,
+  Toast,
+  Root,
   Left,
 } from "native-base";
 import { Image } from "react-native";
@@ -27,76 +29,98 @@ const AddActivity = (props) => {
     setEnteredText(event.target.value);
   };
 
+  const submitPost = () => {
+    Toast.show({
+      text: "You've earned () points!",
+      buttonText: "Okay",
+      position: "bottom",
+    });
+    setTimeout(() => {
+      props.navigation.goBack();
+    }, 1000);
+  };
+
   return (
-    <Container>
-      <Header>
-        <Left>
-          <Button transparent onPress={() => props.navigation.goBack()}>
-            <Icon name="arrow-round-back" />
-          </Button>
-        </Left>
-        <Body>
-          <Title>Add Activity</Title>
-        </Body>
-        <Right>
-          <Icon name="ios-leaf" />
-        </Right>
-      </Header>
-      <LinearGradient
-        colors={["#B5EAD7", "#fff", "#fff", "#B5EAD7"]}
-        style={{ flex: 1 }}
-      >
-        <Content padder>
-          <Card>
-            <Form style={{ padding: 20 }}>
-              <Image
-                source={{
-                  uri:
-                    "https://wp-rocket.me/wp-content/uploads/1/placeholder-feature-image.png",
-                }}
-                style={{ height: 200, width: null, flex: 1 }}
-              />
-              <Text>Select a category:</Text>
-              <Segment>
-                <Button
-                  active={category === 0}
-                  onPress={() => setCategory(0)}
-                  first
-                >
-                  <Icon name="md-happy" />
-                </Button>
-                <Button active={category === 1} onPress={() => setCategory(1)}>
-                  <Icon name="md-contacts" />
-                </Button>
-                <Button active={category === 2} onPress={() => setCategory(2)}>
-                  <Icon name="md-bicycle" />
-                </Button>
-                <Button active={category === 3} onPress={() => setCategory(3)}>
-                  <Icon name="md-flower" />
-                </Button>
-                <Button
-                  active={category === 4}
-                  onPress={() => setCategory(4)}
-                  last
-                >
-                  <Icon name="md-briefcase" />
-                </Button>
-              </Segment>
-              <Textarea
-                rowSpan={4}
-                rounded
-                bordered
-                placeholder="Write a description here!"
-                onChange={textChangeHandler}
-              />
-            </Form>
-            <Button>
-              <Text>Post!</Text>
+    <Root>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent onPress={() => props.navigation.goBack()}>
+              <Icon name="arrow-round-back" />
             </Button>
-          </Card>
-        </Content>
-      </LinearGradient>
-    </Container>
+          </Left>
+          <Body>
+            <Title>Add Activity</Title>
+          </Body>
+          <Right>
+            <Icon name="ios-leaf" />
+          </Right>
+        </Header>
+        <LinearGradient
+          colors={["#B5EAD7", "#fff", "#fff", "#B5EAD7"]}
+          style={{ flex: 1 }}
+        >
+          <Content padder>
+            <Card>
+              <Form style={{ padding: 20 }}>
+                <Image
+                  source={{
+                    uri:
+                      "https://wp-rocket.me/wp-content/uploads/1/placeholder-feature-image.png",
+                  }}
+                  style={{ height: 200, width: null, flex: 1 }}
+                />
+                <Text>Select a category:</Text>
+                <Segment>
+                  <Button
+                    active={category === 0}
+                    onPress={() => setCategory(0)}
+                    first
+                  >
+                    <Icon name="md-happy" />
+                  </Button>
+                  <Button
+                    active={category === 1}
+                    onPress={() => setCategory(1)}
+                  >
+                    <Icon name="md-contacts" />
+                  </Button>
+                  <Button
+                    active={category === 2}
+                    onPress={() => setCategory(2)}
+                  >
+                    <Icon name="md-bicycle" />
+                  </Button>
+                  <Button
+                    active={category === 3}
+                    onPress={() => setCategory(3)}
+                  >
+                    <Icon name="md-flower" />
+                  </Button>
+                  <Button
+                    active={category === 4}
+                    onPress={() => setCategory(4)}
+                    last
+                  >
+                    <Icon name="md-briefcase" />
+                  </Button>
+                </Segment>
+                <Textarea
+                  rowSpan={4}
+                  rounded
+                  bordered
+                  placeholder="Write a description here!"
+                  onChange={textChangeHandler}
+                />
+              </Form>
+              <Button onPress={() => submitPost()}>
+                <Text>Post!</Text>
+              </Button>
+            </Card>
+          </Content>
+        </LinearGradient>
+      </Container>
+    </Root>
   );
 };
 
