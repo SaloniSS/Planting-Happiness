fa9d996a2fb4f5da2f1e2090a909ee8b92270172
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-04-24 23:47:28 -0500

    add in list and card

---
+++ client/components/Post.js
@@ -0,0 +1,61 @@
+import React, { Component } from "react";
+import { Image } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Right,
+} from "native-base";
+
+const Post = (props) => {
+  return (
+    <Container>
+      <Header />
+      <Content>
+        <Card>
+          <CardItem>
+            <Left>
+              <Thumbnail source={{ uri: props.user.profilePic }} />
+              <Body>
+                <Text>{props.user.username}</Text>
+              </Body>
+            </Left>
+            <Right>
+              <Icon name={props.post.category} />
+            </Right>
+          </CardItem>
+          <CardItem cardBody>
+            <Image
+              source={{ uri: props.post.image }}
+              style={{ height: 200, width: null, flex: 1 }}
+            />
+          </CardItem>
+          <CardItem>
+            <Left>
+              <Text>{props.post.date}</Text>
+            </Left>
+            <Right>
+              <Button transparent>
+                <Text>{props.post.likeCount}</Text>
+                <Icon name="md-thumbs-up" />
+              </Button>
+            </Right>
+          </CardItem>
+          <CardItem>
+            <Text>{props.post.description}</Text>
+          </CardItem>
+        </Card>
+      </Content>
+    </Container>
+  );
+};
+
+export default Post;
--- client/screens/Feed.js
+++ client/screens/Feed.js
@@ -1,14 +1,52 @@
 import React from "react";
-import { Text } from "native-base";
-import { View } from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  List,
+  ListItem,
+  Body,
+  Title,
+} from "native-base";
+
+import Post from "../components/Post";
+
+const sampleData = [
+  {
+    user: {
+      profilePic:
+        "https://i.pinimg.com/736x/bf/64/3a/bf643a1d81489917e05e664bfa579595.jpg",
+      username: "btslover7878",
+    },
+    post: {
+      likeCount: 3,
+      description: "I love BTS!!!!",
+      date: "04/23/2020",
+      image:
+        "https://www.kindpng.com/picc/m/123-1236277_jimin-bts-derp-face-png-download-bts-meme.png",
+      category: "md-happy",
+    },
+  },
+];
 
 const Feed = () => {
   return (
-    <View>
-      <View>
-        <Text>Feed</Text>
-      </View>
-    </View>
+    <Container>
+      <Header>
+        <Body>
+          <Title>Feed</Title>
+        </Body>
+      </Header>
+      <Content>
+        <List>
+          {sampleData.map((post) => (
+            <ListItem key={post.post_id}>
+              <Post user={post.user} post={post.post} />
+            </ListItem>
+          ))}
+        </List>
+      </Content>
+    </Container>
   );
 };
 
